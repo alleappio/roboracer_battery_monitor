8f91087bf9166b19c5d64f8b2dec718efdfd4df4
aappio <alle.appio@gmail.com>
2026-06-15 14:36:18 +0200

    add wifi and vescuart to esp
    Signed-off-by: aappio <alle.appio@gmail.com>

--- battery_monitor/src/main.cpp
+++ battery_monitor/src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+#include <VescUart.h>
+#include <WiFi.h>
+
+#include "HardwareSerial.h"
+#include "WiFiType.h"
+#include "esp32-hal-gpio.h"
+#include "parameters.hpp"
+
+WiFiClient wifiClient;
+HardwareSerial vescSerial(1);
+VescUart vescUart;
+
+void setupWifi(){
+    WiFi.begin(ssid, pass);
+    while(WiFi.status() != WL_CONNECTED){
+        delay(500);
+    }
+}
 
 void setup(){
-
+    Serial.begin(115200);
+    vescSerial.begin(115200, SERIAL_8N1, uart_rx, uart_tx);
+    pinMode(LED_BUILTIN, OUTPUT);
+    vescUart.setSerialPort(&vescSerial);
+    setupWifi();    
 }
 
 void loop(){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
 
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
