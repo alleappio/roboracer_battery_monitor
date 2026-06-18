85cc79d4c454ab6fcc707634f6b1a2a6ae91da89
aappio <alle.appio@gmail.com>
2026-06-18 12:34:12 +0200

    add mqtt send rate and put a namespace on parameters
    Signed-off-by: aappio <alle.appio@gmail.com>

--- battery_monitor/include/parameters.example.hpp
+++ battery_monitor/include/parameters.example.hpp
@@ -1,15 +1,18 @@
 #ifndef PARAMETERS_HPP
 #define PARAMETERS_HPP
+namespace Params {
+    constexpr inline int uart_rx = 44;
+    constexpr inline int uart_tx = 43;
 
-constexpr inline int uart_rx = 44;
-constexpr inline int uart_tx = 43;
+    constexpr inline char* ssid = "";
+    constexpr inline char* pass = "";
 
-constexpr inline char* ssid = "";
-constexpr inline char* pass = "";
+    constexpr inline char* broker_ip = "127.0.0.1";
+    constexpr inline int broker_port = 1883;
 
-constexpr inline char* broker_ip = "127.0.0.1";
-constexpr inline int broker_port = 1883;
+    constexpr inline char* mqttId = "";
 
-constexpr inline char* mqttId = "";
+    constexpr inline float mqtt_send_rate = 20; // hz
+}
 
 #endif //PARAMETERS_HPP
--- battery_monitor/src/main.cpp
+++ battery_monitor/src/main.cpp
@@ -16,17 +16,17 @@ VescUart vescUart;
 PubSubClient mqttClient(wifiClient);
 
 void setupWifi(){
-    WiFi.begin(ssid, pass);
+    WiFi.begin(Params::ssid, Params::pass);
     while(WiFi.status() != WL_CONNECTED){
         delay(500);
     }
 }
 
 void setupMqtt(){
-    mqttClient.setServer(broker_ip, broker_port);
+    mqttClient.setServer(Params::broker_ip, Params::broker_port);
     while (!mqttClient.connected()) {
-        mqttClient.connect(mqttId);
-        Serial.printf("connecting to %s:%d\n", broker_ip, broker_port);
+        mqttClient.connect(Params::mqttId);
+        Serial.printf("connecting to %s:%d\n", Params::broker_ip, Params::broker_port);
         delay(100);
     }
     Serial.println("connected to mqtt");
@@ -36,7 +36,7 @@ void sendMessage(char* value_string, float value){
     char message[32];
     char topic[32];
     snprintf(message, sizeof(message), "%.2f", value);
-    snprintf(topic, sizeof(topic), "/%s/%s", mqttId, value_string);
+    snprintf(topic, sizeof(topic), "/%s/%s", Params::mqttId, value_string);
     Serial.printf("%s: %s", topic, message);
     mqttClient.publish(topic, message);
 }
@@ -44,7 +44,7 @@ void sendMessage(char* value_string, float value){
 void setup(){
     Serial.begin(115200);
     Serial.println("helloooo");
-    vescSerial.begin(115200, SERIAL_8N1, uart_rx, uart_tx);
+    vescSerial.begin(115200, SERIAL_8N1, Params::uart_rx, Params::uart_tx);
     pinMode(LED_BUILTIN, OUTPUT);
     vescUart.setSerialPort(&vescSerial);
     setupWifi();
@@ -67,5 +67,6 @@ void loop(){
         Serial.println("Failed to read VESC");
     }
 
-    // delay(1000);
+    float delay_time = 1/Params::mqtt_send_rate;
+    delay(delay_time);
 }
